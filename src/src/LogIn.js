9c12b460170b7ab61c72c74e8d629a3fdb356545
Bruna Cruz <brunavascruz@gmail.com>
2022-10-05 19:12:47 -0300

    react

---
+++ src/src/LogIn.js
@@ -0,0 +1,14 @@
+import './LogIn.css';
+import LogInComponent from './components/LogInComponent';
+import CadastroComponent from './components/CadastroComponent';
+
+function LogIn() {
+  return (
+    <div className="LogIn">
+      <LogInComponent/>
+      <CadastroComponent/>
+    </div>
+  );
+}
+
+export default LogIn;
---
+++ src/src/components/LogInComponent.js
@@ -0,0 +1,46 @@
+import CadastroComponent from "./CadastroComponent";
+
+function LogInComponent() {
+
+    const handleClickCadastro = () => {
+        CadastroComponent();
+    }
+
+
+    return(
+      
+    <div className="body login">
+        <div className="inner login">
+            <div className="container login" id="container">
+                <div className="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Login</h1>
+                        <input type="email" placeholder="Email" id="email"/>
+                        <input type="password" placeholder="Senha" id="senha"/>
+                        <a href="senha.html">Esqueceu sua senha?</a>
+                        <button>Entrar</button>
+                    </form>
+                </div>
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-right">
+                            <h1>Olá, Agrônomo MMM!</h1>
+                            <p>Faça aqui o seu cadastro no nosso sistema!</p>
+                            <a id="register-button" href="#"><button onClick={handleClickCadastro} className="ghost">Cadastro</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <footer>
+        <p>
+            Acesse o site da Muda Meu Mundo (MMM) para conhecer 
+            <a target="_blank" href="https://www.mudameumundo.com.br/"> mais</a>
+        </p>
+    </footer>
+            </div>
+    )
+}
+
+export default LogInComponent
