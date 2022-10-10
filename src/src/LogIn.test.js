341de433660a98a6dc8fc54432a2420f12d3b78e
Bruna Cruz <brunavascruz@gmail.com>
2022-10-10 15:37:03 -0300

    deleted folder

--- src/src/LogIn.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import LogIn from './LogIn';
-
-test('renders learn react link', () => {
-  render(<LogIn />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
